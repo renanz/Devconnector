fd73df62c1c1dc48368126824af94cd6c05f21f7
Renan Zelaya <renanzelaya@acklenavenue.com>
2019-04-20 14:13:56 -0600

    create/edit profile route

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -36,13 +36,71 @@ router.get(
 );
 
 // @route   POST api/profile
-// @desc    Create user profile
+// @desc    Create or edit user profile
 // @access  Private
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    const errors = {};
+    // Get Fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    const {
+      handle,
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      linkedin,
+      facebook,
+      instagram,
+      twitter
+    } = req.body;
+
+    if (handle) profileFields.handle = handle;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    // Skills - split into array
+    if (typeof skills !== "undefined") profileFields.skills = skills.split(",");
+    // Socials
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (twitter) profileFields.social.twitter = twitter;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Update
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        // Create
+
+        // Check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "That handle already exists";
+            res.status(400).json(errors);
+          }
+        });
+
+        // Save Profile
+        new Profile(profileFields).save().then(profile => res.json(profile));
+      }
+    });
   }
 );
 
