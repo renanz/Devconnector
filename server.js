8fe516715000c280a70aec130a48cddc09bad4b3
Renan Zelaya <renanzelaya@acklenavenue.com>
2019-04-20 13:51:52 -0600

    renamed profile route

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ require("./config/passport")(passport);
 
 // Use routes
 app.use("/api/users", users);
-app.use("/api/profiles", profiles);
+app.use("/api/profile", profiles);
 app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
