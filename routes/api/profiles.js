879cd2f6caf3710d28c2a3f68a8ead1ede1df310
Renan Zelaya <renanzelaya@acklenavenue.com>
2019-04-20 13:54:39 -0600

    user profile route

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load Profile model
+const Profile = require("../../models/Profile");
+// Load User model
+const User = require("../../models/User");
 
 // @route   GET api/profiles/test
 // @desc    Test profiles route
@@ -7,5 +14,36 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({ msg: "Profiles works" });
 });
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Public
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   POST api/profile
+// @desc    Create user profile
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+  }
+);
 
 module.exports = router;
