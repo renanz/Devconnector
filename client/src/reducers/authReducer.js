fb40c8a4da7ee7fa9b69f9fd524b11a2778e0826
Renan Zelaya <renanzelaya@acklenavenue.com>
2019-04-21 13:58:18 -0600

    added jwt decoder and added action/reducer for login

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,4 +1,6 @@
-// import { TEST_DISPATCH } from "../actions/types";
+import { SET_CURRENT_USER } from "../actions/types";
+
+import isEmpty from "../validation/is-empty";
 
 const initialState = {
   isAuthenticated: false,
@@ -7,6 +9,12 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
     default:
       return state;
   }
