ab607ccae4a8fe4ed1ecf88cc04cfc5926e9fe21
Renan Zelaya <renanzelaya@acklenavenue.com>
2019-04-21 18:58:40 -0600

    actions, reducers and types for profiles

---
+++ client/src/actions/profileActions.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS } from "./types";
+
+// Get Current Profile
+export const getCurrentProfile = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile")
+    .then(res => dispatch({ type: GET_PROFILE, payload: res.data }))
+    .catch(err => dispatch({ type: GET_PROFILE, payload: {} }));
+};
+
+// Profile Loading
+export const setProfileLoading = () => {
+  return { type: PROFILE_LOADING };
+};
